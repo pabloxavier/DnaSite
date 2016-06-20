de9f78039c03616f979c6b38cb2586c3bc620825
Luis Rogerio <lr.developer@outlook.com>
2016-06-20 17:00:50 -0300

    Commit VS LR-Work (View Logged, CSS Transition) - 20-06-2016

---
+++ DNAMais.Site/js/contractServices.js
@@ -0,0 +1,359 @@
+﻿$(document).ready(function () {
+
+    $('#btnCpf, #btnAddr, #btnName, #btnTel').on('click', function (e) {
+
+        var idClick = e.target.id;
+
+        if (idClick == "btnCpf") {
+
+            $('#divPj').fadeOut(500);
+            $('#divVehicle').fadeOut(500);
+            $('#divFtp').fadeOut(500);
+            $('div.grid-pf').addClass('test-grid');
+            $('#divPf').removeClass('effect-sadie');
+            $('#divPf').addClass('effect-sadie-2');
+            $('#btnBack').fadeIn(800);
+
+            setTimeout(function () {
+
+                $('#pfCpf').fadeIn(500);
+                $('#pfAddr').hide();
+                $('#pfName').hide();
+                $('#pfTel').hide();
+                $('#txtCpf').focus();
+
+            }, 500);
+
+            $('#btnCpf, #btnAddr, #btnName, #btnTel').on('click', function (e) {
+
+                var idClick = e.target.id;
+
+                if (idClick == "btnCpf") {
+
+                    setTimeout(function () {
+
+                        $('#pfCpf').fadeIn(500);
+                        $('#pfAddr').hide();
+                        $('#pfName').hide();
+                        $('#pfTel').hide();
+                        $('#txtCpf').focus();
+
+                    }, 10);
+                }
+
+                else if (idClick == "btnAddr") {
+
+                    setTimeout(function () {
+
+                        $('#pfAddr').fadeIn(500);
+                        $('#pfCpf').hide();
+                        $('#pfName').hide();
+                        $('#pfTel').hide();
+                        $('#txtAddr').focus();
+
+                    }, 10);
+                }
+
+                else if (idClick == "btnName") {
+
+                    setTimeout(function () {
+
+                        $('#pfName').fadeIn(500);
+                        $('#pfCpf').hide();
+                        $('#pfAddr').hide();
+                        $('#pfTel').hide();
+                        $('#txtName').focus();
+
+                    }, 10);
+                }
+
+                else if (idClick == "btnTel") {
+
+                    setTimeout(function () {
+
+                        $('#pfTel').fadeIn(500);
+                        $('#pfCpf').hide();
+                        $('#pfName').hide();
+                        $('#pfAddr').hide();
+                        $('#txtTel').focus();
+
+                    }, 10);
+                }
+
+            });
+        }
+
+
+        else if (idClick == "btnAddr") {
+
+            $('#divPj').fadeOut(500);
+            $('#divVehicle').fadeOut(500);
+            $('#divFtp').fadeOut(500);
+            $('div.grid-pf').addClass('test-grid');
+            $('#divPf').removeClass('effect-sadie');
+            $('#divPf').addClass('effect-sadie-2');
+            $('#btnBack').fadeIn(800);
+
+            setTimeout(function () {
+
+                $('#pfAddr').fadeIn(500);
+                $('#pfCpf').hide();
+                $('#pfName').hide();
+                $('#pfTel').hide();
+                $('#txtAddr').focus();
+
+            }, 500);
+
+            $('#btnCpf, #btnAddr, #btnName, #btnTel').on('click', function (e) {
+
+                var idClick = e.target.id;
+
+                if (idClick == "btnCpf") {
+
+                    setTimeout(function () {
+
+                        $('#pfCpf').fadeIn(500);
+                        $('#pfAddr').hide();
+                        $('#pfName').hide();
+                        $('#pfTel').hide();
+                        $('#txtCpf').focus();
+
+                    }, 10);
+                }
+
+                else if (idClick == "btnAddr") {
+
+                    setTimeout(function () {
+
+                        $('#pfAddr').fadeIn(500);
+                        $('#pfCpf').hide();
+                        $('#pfName').hide();
+                        $('#pfTel').hide();
+                        $('#txtAddr').focus();
+
+                    }, 10);
+                }
+
+                else if (idClick == "btnName") {
+
+                    setTimeout(function () {
+
+                        $('#pfName').fadeIn(500);
+                        $('#pfCpf').hide();
+                        $('#pfAddr').hide();
+                        $('#pfTel').hide();
+                        $('#txtName').focus();
+
+                    }, 10);
+                }
+
+                else if (idClick == "btnTel") {
+
+                    setTimeout(function () {
+
+                        $('#pfTel').fadeIn(500);
+                        $('#pfCpf').hide();
+                        $('#pfName').hide();
+                        $('#pfAddr').hide();
+                        $('#txtTel').focus();
+
+                    }, 10);
+                }
+
+            });
+
+        }
+
+        else if (idClick == "btnName") {
+
+            $('#divPj').fadeOut(500);
+            $('#divVehicle').fadeOut(500);
+            $('#divFtp').fadeOut(500);
+            $('div.grid-pf').addClass('test-grid');
+            $('#divPf').removeClass('effect-sadie');
+            $('#divPf').addClass('effect-sadie-2');
+            $('#btnBack').fadeIn(800);
+
+            setTimeout(function () {
+
+                $('#pfName').fadeIn(500);
+                $('#pfCpf').hide();
+                $('#pfAddr').hide();
+                $('#pfTel').hide();
+                $('#txtName').focus();
+
+            }, 500);
+
+            $('#btnCpf, #btnAddr, #btnName, #btnTel').on('click', function (e) {
+
+                var idClick = e.target.id;
+
+                if (idClick == "btnCpf") {
+
+                    setTimeout(function () {
+
+                        $('#pfCpf').fadeIn(500);
+                        $('#pfAddr').hide();
+                        $('#pfName').hide();
+                        $('#pfTel').hide();
+                        $('#txtCpf').focus();
+
+                    }, 10);
+                }
+
+                else if (idClick == "btnAddr") {
+
+                    setTimeout(function () {
+
+                        $('#pfAddr').fadeIn(500);
+                        $('#pfCpf').hide();
+                        $('#pfName').hide();
+                        $('#pfTel').hide();
+                        $('#txtAddr').focus();
+
+                    }, 10);
+                }
+
+                else if (idClick == "btnName") {
+
+                    setTimeout(function () {
+
+                        $('#pfName').fadeIn(500);
+                        $('#pfCpf').hide();
+                        $('#pfAddr').hide();
+                        $('#pfTel').hide();
+                        $('#txtName').focus();
+
+                    }, 10);
+                }
+
+                else if (idClick == "btnTel") {
+
+                    setTimeout(function () {
+
+                        $('#pfTel').fadeIn(500);
+                        $('#pfCpf').hide();
+                        $('#pfName').hide();
+                        $('#pfAddr').hide();
+                        $('#txtTel').focus();
+
+                    }, 10);
+                }
+
+            });
+
+        }
+
+        else if (idClick == "btnTel") {
+
+            $('#divPj').fadeOut(500);
+            $('#divVehicle').fadeOut(500);
+            $('#divFtp').fadeOut(500);
+            $('div.grid-pf').addClass('test-grid');
+            $('#divPf').removeClass('effect-sadie');
+            $('#divPf').addClass('effect-sadie-2');
+            $('#btnBack').fadeIn(800);
+
+            setTimeout(function () {
+
+                $('#pfTel').fadeIn(500);
+                $('#pfCpf').hide();
+                $('#pfName').hide();
+                $('#pfAddr').hide();
+                $('#txtTel').focus();
+
+            }, 500);
+
+            $('#btnCpf, #btnAddr, #btnName, #btnTel').on('click', function (e) {
+
+                var idClick = e.target.id;
+
+                if (idClick == "btnCpf") {
+
+                    setTimeout(function () {
+
+                        $('#pfCpf').fadeIn(500);
+                        $('#pfAddr').hide();
+                        $('#pfName').hide();
+                        $('#pfTel').hide();
+                        $('#txtCpf').focus();
+
+                    }, 10);
+                }
+
+                else if (idClick == "btnAddr") {
+
+                    setTimeout(function () {
+
+                        $('#pfAddr').fadeIn(500);
+                        $('#pfCpf').hide();
+                        $('#pfName').hide();
+                        $('#pfTel').hide();
+                        $('#txtAddr').focus();
+
+                    }, 10);
+                }
+
+                else if (idClick == "btnName") {
+
+                    setTimeout(function () {
+
+                        $('#pfName').fadeIn(500);
+                        $('#pfCpf').hide();
+                        $('#pfAddr').hide();
+                        $('#pfTel').hide();
+                        $('#txtName').focus();
+
+                    }, 10);
+                }
+
+                else if (idClick == "btnTel") {
+
+                    setTimeout(function () {
+
+                        $('#pfTel').fadeIn(500);
+                        $('#pfCpf').hide();
+                        $('#pfName').hide();
+                        $('#pfAddr').hide();
+                        $('#txtTel').focus();
+
+                    }, 10);
+                }
+
+            });
+
+        }
+
+
+
+    });
+
+
+
+
+
+    $('#btnBack').on('click', function () {
+
+        $('p').hide();
+        $('#divPf').removeClass('effect-sadie-2');
+        $('div.grid-pf').removeClass('test-grid');
+        $('#divPf').addClass('effect-sadie');
+        $('#divPj').delay(1000).fadeIn(1000);
+        $('#divVehicle').delay(1000).fadeIn(1000);
+        $('#divFtp').delay(1000).fadeIn(1000);
+        $('#pfCpf').fadeOut(800);
+        $('#pfAddr').fadeOut(800);
+        $('#pfName').fadeOut(800);
+        $('#pfTel').fadeOut(800);
+        $('#btnBack').fadeOut(800);
+
+        setTimeout(function () {
+
+            $('p').show();
+
+        }, 1000);
+
+
+    });
+
+});
