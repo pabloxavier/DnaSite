92b2fcf3afa97a6fa67149bc86ce3c3727e13883
Luis Rogerio <lr.developer@outlook.com>
2016-07-15 17:02:07 -0300

    Commit VS LR-Work (Refazendo página de serviços para views separadas) - 15-07-2016

--- DNAMais.Site/js/contractServicesPf.js
+++ DNAMais.Site/js/contractServicesPf.js
@@ -1,116 +1,116 @@
-﻿$(document).ready(function () {
+﻿//$(document).ready(function () {
 
-    //Pessoa Física
-    //Clique em CPF, Endereço, Nome ou Telefone
-    $('#btnCpf, #btnAddr, #btnName, #btnTel').on('click', function (e) {
+//    //Pessoa Física
+//    //Clique em CPF, Endereço, Nome ou Telefone
+//    $('#btnCpf, #btnAddr, #btnName, #btnTel').on('click', function (e) {
 
-        var idClick = e.target.id;
+//        var idClick = e.target.id;
 
-        //Clicando em CPF
-        if (idClick == "btnCpf") {
+//        //Clicando em CPF
+//        if (idClick == "btnCpf") {
 
-            pfTransitionEffect();
+//            pfTransitionEffect();
 
-            setTimeout(function () {
+//            setTimeout(function () {
 
-                $('#pfAddr,#pfName,#pfTel').hide();
-                $('#pfCpf').fadeIn(800);
-                $('#txtCpf').focus();
+//                $('#pfAddr,#pfName,#pfTel').hide();
+//                $('#pfCpf').fadeIn(800);
+//                $('#txtCpf').focus();
 
-            }, 1000);
+//            }, 1000);
 
-        }
+//        }
 
-        //Clicando em Endereço
-        else if (idClick == "btnAddr") {
+//        //Clicando em Endereço
+//        else if (idClick == "btnAddr") {
 
-            pfTransitionEffect();
+//            pfTransitionEffect();
 
-            setTimeout(function () {
+//            setTimeout(function () {
 
-                $('#pfCpf,#pfName,#pfTel').hide();
-                $('#pfAddr').fadeIn(800);
-                $('#txtAddr').focus();
+//                $('#pfCpf,#pfName,#pfTel').hide();
+//                $('#pfAddr').fadeIn(800);
+//                $('#txtAddr').focus();
 
-            }, 1000);
+//            }, 1000);
 
-        }
+//        }
 
-        //Clicando em Nome
-        else if (idClick == "btnName") {
+//        //Clicando em Nome
+//        else if (idClick == "btnName") {
 
-            pfTransitionEffect();
+//            pfTransitionEffect();
 
-            setTimeout(function () {
+//            setTimeout(function () {
 
-                $('#pfCpf,#pfAddr,#pfTel').hide();
-                $('#pfName').fadeIn(800);
-                $('#txtName').focus();
+//                $('#pfCpf,#pfAddr,#pfTel').hide();
+//                $('#pfName').fadeIn(800);
+//                $('#txtName').focus();
 
-            }, 1000);
+//            }, 1000);
 
-        }
+//        }
 
-        //Clicando em Telefone
-        else if (idClick == "btnTel") {
+//        //Clicando em Telefone
+//        else if (idClick == "btnTel") {
 
-            pfTransitionEffect();
+//            pfTransitionEffect();
 
-            setTimeout(function () {
+//            setTimeout(function () {
 
-                $('#pfCpf,#pfName,#pfAddr').hide();
-                $('#pfTel').fadeIn(800);
-                $('#txtTel').focus();
+//                $('#pfCpf,#pfName,#pfAddr').hide();
+//                $('#pfTel').fadeIn(800);
+//                $('#txtTel').focus();
 
-            }, 1000);
+//            }, 1000);
 
-        }
+//        }
 
-    });
+//    });
 
-    //Fase Dois
-    $('#btnCpfPhaseTwo, #btnAddrPhaseTwo, #btnNamePhaseTwo, #btnTelPhaseTwo').on('click', function (e) {
+//    //Fase Dois
+//    $('#btnCpfPhaseTwo, #btnAddrPhaseTwo, #btnNamePhaseTwo, #btnTelPhaseTwo').on('click', function (e) {
 
-        var idClick = e.target.id;
+//        var idClick = e.target.id;
 
-        if (idClick == "btnCpfPhaseTwo") {
+//        if (idClick == "btnCpfPhaseTwo") {
 
-            cpfClickPhaseTwo();
+//            cpfClickPhaseTwo();
 
-        }
+//        }
 
-        else if (idClick == "btnAddrPhaseTwo") {
+//        else if (idClick == "btnAddrPhaseTwo") {
 
-            addressClickPhaseTwo();
+//            addressClickPhaseTwo();
 
-        }
+//        }
 
-        else if (idClick == "btnNamePhaseTwo") {
+//        else if (idClick == "btnNamePhaseTwo") {
 
-            nameClickPhaseTwo();
+//            nameClickPhaseTwo();
 
-        }
+//        }
 
-        else if (idClick == "btnTelPhaseTwo") {
+//        else if (idClick == "btnTelPhaseTwo") {
 
-            telClickPhaseTwo();
+//            telClickPhaseTwo();
 
-        }
+//        }
 
-    });
+//    });
 
-    //Botão Voltar
-    $('#btnBack').on('click', function () {
+//    //Botão Voltar
+//    $('#btnBack').on('click', function () {
 
-        $('#btnPhaseTwo').hide();
-        $('#btnDefault').show();
-        $('#divPf').removeClass('effect-sadie-2');
-        $('div.grid-pf').removeClass('grid-pf-after');
-        $('#divPf').addClass('effect-sadie');
-        $('#divPj,#divVehicle,#divFtp').delay(1000).fadeIn(500);
-        $('#pfCpf,#pfAddr,#pfName,#pfTel').hide();
-        $('#btnBack').hide();
+//        $('#btnPhaseTwo').hide();
+//        $('#btnDefault').show();
+//        $('#divPf').removeClass('effect-sadie-2');
+//        $('div.grid-pf').removeClass('grid-pf-after');
+//        $('#divPf').addClass('effect-sadie');
+//        $('#divPj,#divVehicle,#divFtp').delay(1000).fadeIn(500);
+//        $('#pfCpf,#pfAddr,#pfName,#pfTel').hide();
+//        $('#btnBack').hide();
 
-    });
+//    });
 
-});
+//});
